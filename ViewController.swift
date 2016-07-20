c8ea920a736e036c96e1fbd6a80ba7a966bcf54c
ADITYA <aditya.h@emtres.co.id>
2016-07-20 09:08:07 +0800

    add button press action and linked it to play btn

--- ViewController.swift
+++ ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn:UIButton!
     @IBOutlet weak var tabLbl:UILabel!
     
-    
+    @IBAction func onPlayButtonPressed(sender:UIButton!){
+        
+    }
     
 
 }
