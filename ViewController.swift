63f43da64a82ac6aabe49441dd6dbe22907a9fed
ADITYA <aditya.h@emtres.co.id>
2016-07-21 08:46:48 +0800

    add taps variables

--- ViewController.swift
+++ ViewController.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Properties
+    var maxTaps:Int = 0
+    var currentTaps:Int = 0
     
+    // Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTF: UITextField!
     @IBOutlet weak var playBtn: UIButton!
     
     @IBOutlet weak var tapBtn:UIButton!
-    @IBOutlet weak var tabLbl:UILabel!
+    @IBOutlet weak var tapLbl:UILabel!
     
     @IBAction func onPlayButtonPressed(sender:UIButton!){
+        logoImg.hidden = true;
+        howManyTapsTF.hidden = true
+        playBtn.hidden = true
         
+        tapBtn.hidden = false
+        tapLbl.hidden = false
     }
     
 
